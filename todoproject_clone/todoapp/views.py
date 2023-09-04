3b129ca7e329bc0943a915008ffcf08dbfa03533
ads0909 <88043069+pickry@users.noreply.github.com>
2023-09-04 19:02:55 +0530

    Add files via upload

---
+++ todoproject_clone/todoapp/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import TodoListItem
+from django.http import HttpResponseRedirect
+# Create your views here.
+def home(request):
+    all_todo_items = TodoListItem.objects.all()
+    return render(request,'index.html',{'all_items':all_todo_items})
+def addTodoView(request):
+    x = request.POST['content']
+    new_item = TodoListItem(content = x)
+    new_item.save()
+    return HttpResponseRedirect('/') 
+def deleteTodoView(request,i):
+    y = TodoListItem.objects.get(id= i)
+    y.delete()
+    return HttpResponseRedirect('/') 
+def deletelist(request):
+    y = TodoListItem.objects.all()
+    y.delete()
+    return HttpResponseRedirect('/')
